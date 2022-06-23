862c15e2fac8eeca05727972b3399f1e4a05ea35
dcarr <dcarr@DESKTOP-1URGJ1L>
2022-06-24 01:23:06 +0200

    commit prueba

---
+++ src/main/java/com/rfranco/paquetes/PaquetesApplication.java
@@ -0,0 +1,13 @@
+package com.rfranco.paquetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaquetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaquetesApplication.class, args);
+	}
+
+}
